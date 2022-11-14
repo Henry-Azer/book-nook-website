56a7b897ab6683754fe921525106acb8d9a2ffe4
unknown <a_bavly@yahoo.com>
2022-11-14 20:17:28 +0200

    Edit login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,6 +3,7 @@ import Circle from "./circle";
 
 import logo_white from "../assets/images/logo-white.png";
 import book_mark from "../assets/images/Asset-1.png";
+import { Link } from "react-router-dom";
 
 export default function login() {
      return (
@@ -10,9 +11,16 @@ export default function login() {
                <Circle
                     size="200"
                     backgroundColor="beige"
-                    duration="16"
+                    duration="25"
                     top="10"
                     left="-150"
+               />
+               <Circle
+                    size="300"
+                    backgroundColor="light-purple"
+                    duration="30"
+                    bottom="-100"
+                    left="-120"
                />
                <Circle
                     size="300"
@@ -22,19 +30,17 @@ export default function login() {
                     right="-200"
                />
 
-               <div className="h-100 w-100 d-flex justify-content-center align-items-center">
-                    <div className="d-flex align-items-center login-form w-50 border border-1 rounded-3 light-purple-border text-white flex-column p-3">
-                         <img
-                              src={logo_white}
-                              alt="logo"
-                              style={{ width: "150px" }}
-                         />
-                         <h1 className="mt-4">Sign in</h1>
-                         <p className="mt-4 fs-4 text-center">
-                              What are you waiting for ...? a lot of books is
-                              waiting for you don't miss the chance sign in now
-                         </p>
-                         <div className="d-flex flex-column justify-content-around align-items-center w-100 h-100">
+               <div className="h-100 w-75 mx-auto d-flex justify-content-center align-items-center">
+                    <div className="d-flex align-items-center justify-content-center login-form w-50 border border-1 rounded-3 light-purple-border text-white flex-column p-3">
+                         <Link to="/home">
+                              <img
+                                   src={logo_white}
+                                   alt="logo"
+                                   style={{ width: "150px" }}
+                              />
+                         </Link>
+                         <h1 className="mt-4">Login</h1>
+                         <div className="d-flex flex-column justify-content-around align-items-center w-100 h-50">
                               <input
                                    type="text"
                                    className="w-75 rounded-5 form-control light-purple-border text-white"
@@ -45,8 +51,8 @@ export default function login() {
                                    className="w-75 rounded-5 form-control light-purple-border text-white"
                                    placeholder="Password"
                               />
-                              <button className="btn btn-hover text-white position-relative rounded-5 w-75 light-purple-background d-block mx-auto">
-                                   Sign in{" "}
+                              <button className="btn btn-hover text-white position-relative rounded-5 w-50 light-purple-background d-block mx-auto">
+                                   Login
                                    <img
                                         src={book_mark}
                                         alt="book_mark"
@@ -58,9 +64,9 @@ export default function login() {
                                         }}
                                    />
                               </button>
-                              <p className="text-decoration-underline">
+                              <Link to='/register' className="link-hover text-white">
                                    Create a new account ?
-                              </p>
+                              </Link>
                          </div>
                     </div>
                </div>
